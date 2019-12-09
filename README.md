75e07f1604697047897bff5474f0710d3543a3ab
Jenaro <jen.calvineo@gmail.com>
2019-12-09 16:55:12 -0300

    Added functionality to io

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 Useless but awesome Effects/Tools/Whatever for devs
 
+# Development
+
+```npm run dev```
+
+# Build
+
+```npm run build```
--- dist/ules.js
+++ dist/ules.js
@@ -31,42 +31,109 @@
     }
   }
 
-  var css = ":root {\n  --easing: cubic-bezier(0.390, 0.575, 0.565, 1.000);\n  --duration: 1s;\n  --animation-name: fade-in;\n}\n\n.animate {\n\t-webkit-animation: var(--animation-name) var(--duration) var(--easing);\n\t        animation: var(--animation-name) var(--duration) var(--easing);\n}\n\n@-webkit-keyframes fade-in{0%{opacity:0}100%{opacity:1}}@keyframes fade-in{0%{opacity:0}100%{opacity:1}}\n@-webkit-keyframes fade-in-bottom{0%{-webkit-transform:translateY(50px);transform:translateY(50px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}@keyframes fade-in-bottom{0%{-webkit-transform:translateY(50px);transform:translateY(50px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}\n@-webkit-keyframes fade-in-top{0%{-webkit-transform:translateY(-50px);transform:translateY(-50px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}@keyframes fade-in-top{0%{-webkit-transform:translateY(-50px);transform:translateY(-50px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}\n@-webkit-keyframes fade-in-left{0%{-webkit-transform:translateX(-50px);transform:translateX(-50px);opacity:0}100%{-webkit-transform:translateX(0);transform:translateX(0);opacity:1}}@keyframes fade-in-left{0%{-webkit-transform:translateX(-50px);transform:translateX(-50px);opacity:0}100%{-webkit-transform:translateX(0);transform:translateX(0);opacity:1}}\n@-webkit-keyframes fade-in-right{0%{-webkit-transform:translateX(50px);transform:translateX(50px);opacity:0}100%{-webkit-transform:translateX(0);transform:translateX(0);opacity:1}}@keyframes fade-in-right{0%{-webkit-transform:translateX(50px);transform:translateX(50px);opacity:0}100%{-webkit-transform:translateX(0);transform:translateX(0);opacity:1}}\n@-webkit-keyframes fade-in-fwd{0%{-webkit-transform:translateZ(-80px);transform:translateZ(-80px);opacity:0}100%{-webkit-transform:translateZ(0);transform:translateZ(0);opacity:1}}@keyframes fade-in-fwd{0%{-webkit-transform:translateZ(-80px);transform:translateZ(-80px);opacity:0}100%{-webkit-transform:translateZ(0);transform:translateZ(0);opacity:1}}\n@-webkit-keyframes fade-in-bck{0%{-webkit-transform:translateZ(80px);transform:translateZ(80px);opacity:0}100%{-webkit-transform:translateZ(0);transform:translateZ(0);opacity:1}}@keyframes fade-in-bck{0%{-webkit-transform:translateZ(80px);transform:translateZ(80px);opacity:0}100%{-webkit-transform:translateZ(0);transform:translateZ(0);opacity:1}}\n@-webkit-keyframes fade-in-br{0%{-webkit-transform:translateX(50px) translateY(50px);transform:translateX(50px) translateY(50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}@keyframes fade-in-br{0%{-webkit-transform:translateX(50px) translateY(50px);transform:translateX(50px) translateY(50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}\n@-webkit-keyframes fade-in-bl{0%{-webkit-transform:translateX(-50px) translateY(50px);transform:translateX(-50px) translateY(50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}@keyframes fade-in-bl{0%{-webkit-transform:translateX(-50px) translateY(50px);transform:translateX(-50px) translateY(50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}\n@-webkit-keyframes fade-in-tr{0%{-webkit-transform:translateX(50px) translateY(-50px);transform:translateX(50px) translateY(-50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}@keyframes fade-in-tr{0%{-webkit-transform:translateX(50px) translateY(-50px);transform:translateX(50px) translateY(-50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}\n@-webkit-keyframes fade-in-tl{0%{-webkit-transform:translateX(-50px) translateY(-50px);transform:translateX(-50px) translateY(-50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}@keyframes fade-in-tl{0%{-webkit-transform:translateX(-50px) translateY(-50px);transform:translateX(-50px) translateY(-50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}\n";
+  var css = ":root {\n  --easing: cubic-bezier(0.390, 0.575, 0.565, 1.000);\n  --duration: 1s;\n  --animation-name: fade-in;\n}\n\n.animate {\n\t-webkit-animation: var(--animation-name) var(--duration) var(--easing);\n\t        animation: var(--animation-name) var(--duration) var(--easing);\n}\n\n.ules-io {\n  opacity: 0;\n}\n\n.ules-io.ules-io-done {\n  opacity: 1;\n}\n\n@-webkit-keyframes fade-in{0%{opacity:0}100%{opacity:1}}@keyframes fade-in{0%{opacity:0}100%{opacity:1}}\n@-webkit-keyframes fade-in-bottom{0%{-webkit-transform:translateY(50px);transform:translateY(50px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}@keyframes fade-in-bottom{0%{-webkit-transform:translateY(50px);transform:translateY(50px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}\n@-webkit-keyframes fade-in-top{0%{-webkit-transform:translateY(-50px);transform:translateY(-50px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}@keyframes fade-in-top{0%{-webkit-transform:translateY(-50px);transform:translateY(-50px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}\n@-webkit-keyframes fade-in-left{0%{-webkit-transform:translateX(-50px);transform:translateX(-50px);opacity:0}100%{-webkit-transform:translateX(0);transform:translateX(0);opacity:1}}@keyframes fade-in-left{0%{-webkit-transform:translateX(-50px);transform:translateX(-50px);opacity:0}100%{-webkit-transform:translateX(0);transform:translateX(0);opacity:1}}\n@-webkit-keyframes fade-in-right{0%{-webkit-transform:translateX(50px);transform:translateX(50px);opacity:0}100%{-webkit-transform:translateX(0);transform:translateX(0);opacity:1}}@keyframes fade-in-right{0%{-webkit-transform:translateX(50px);transform:translateX(50px);opacity:0}100%{-webkit-transform:translateX(0);transform:translateX(0);opacity:1}}\n@-webkit-keyframes fade-in-fwd{0%{-webkit-transform:translateZ(-80px);transform:translateZ(-80px);opacity:0}100%{-webkit-transform:translateZ(0);transform:translateZ(0);opacity:1}}@keyframes fade-in-fwd{0%{-webkit-transform:translateZ(-80px);transform:translateZ(-80px);opacity:0}100%{-webkit-transform:translateZ(0);transform:translateZ(0);opacity:1}}\n@-webkit-keyframes fade-in-bck{0%{-webkit-transform:translateZ(80px);transform:translateZ(80px);opacity:0}100%{-webkit-transform:translateZ(0);transform:translateZ(0);opacity:1}}@keyframes fade-in-bck{0%{-webkit-transform:translateZ(80px);transform:translateZ(80px);opacity:0}100%{-webkit-transform:translateZ(0);transform:translateZ(0);opacity:1}}\n@-webkit-keyframes fade-in-br{0%{-webkit-transform:translateX(50px) translateY(50px);transform:translateX(50px) translateY(50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}@keyframes fade-in-br{0%{-webkit-transform:translateX(50px) translateY(50px);transform:translateX(50px) translateY(50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}\n@-webkit-keyframes fade-in-bl{0%{-webkit-transform:translateX(-50px) translateY(50px);transform:translateX(-50px) translateY(50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}@keyframes fade-in-bl{0%{-webkit-transform:translateX(-50px) translateY(50px);transform:translateX(-50px) translateY(50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}\n@-webkit-keyframes fade-in-tr{0%{-webkit-transform:translateX(50px) translateY(-50px);transform:translateX(50px) translateY(-50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}@keyframes fade-in-tr{0%{-webkit-transform:translateX(50px) translateY(-50px);transform:translateX(50px) translateY(-50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}\n@-webkit-keyframes fade-in-tl{0%{-webkit-transform:translateX(-50px) translateY(-50px);transform:translateX(-50px) translateY(-50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}@keyframes fade-in-tl{0%{-webkit-transform:translateX(-50px) translateY(-50px);transform:translateX(-50px) translateY(-50px);opacity:0}100%{-webkit-transform:translateX(0) translateY(0);transform:translateX(0) translateY(0);opacity:1}}\n";
   styleInject(css);
 
-  function io() {
+  function io(_ref) {
+    var direction = _ref.direction,
+        duration = _ref.duration,
+        easing = _ref.easing,
+        threshold = _ref.threshold;
+
+    if (!threshold) {
+      threshold = 0.2;
+    }
+
     var options = {
+      root: null,
       rootMargin: '0px',
-      threshold: 0.2
+      threshold: threshold
     };
-    var observer = new IntersectionObserver(callback, options);
-    var itemsToObserve = document.querySelectorAll('.lazy');
+    var itemsToObserve = document.querySelectorAll('.ules-io');
+    var observer;
 
     function callback(entries, observer) {
       entries.forEach(function (entry) {
         if (entry.intersectionRatio > 0) {
+          if (entry.target.tagName === 'IMG' && entry.target.dataset.src) {
+            entry.target.src = entry.target.dataset.src;
+          }
+
           entry.target.classList.add('animate');
+          setTimeout(function () {
+            return entry.target.classList.add('ules-io-done');
+          }, duration ? duration : 1000);
           observer.unobserve(entry.target);
-        } else {
-          console.log('Not intersecting');
         }
       });
     }
 
+    if (!('IntersectionObserver' in window) || !('IntersectionObserverEntry' in window) || !('intersectionRatio' in window.IntersectionObserverEntry.prototype)) {
+      var debounce = function debounce(func) {
+        var wait = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 20;
+        var immediate = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
+        var timeout;
+        return function () {
+          var context = this,
+              args = arguments;
+
+          var later = function later() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+          };
+
+          var callNow = immediate && !timeout;
+          clearTimeout(timeout);
+          timeout = setTimeout(later, wait);
+          if (callNow) func.apply(context, args);
+        };
+      };
+
+      var checkSlide = function checkSlide(e) {
+        itemsToObserve.forEach(function (item) {
+          var slideInAt = window.scrollY + window.innerHeight - item.height * threshold;
+          var isShown = slideInAt > item.offsetTop;
+
+          if (isShown) {
+            if (entry.target.tagName === 'IMG' && entry.target.dataset.src) {
+              entry.target.src = entry.target.dataset.src;
+            }
+
+            item.classList.add('animate');
+            setTimeout(function () {
+              return item.classList.add('ules-io-done');
+            }, duration ? duration : 1000);
+          }
+        });
+      };
+
+      window.addEventListener('scroll', debounce(checkSlide));
+    } else {
+      observer = new IntersectionObserver(callback, options);
+    }
+
     itemsToObserve.forEach(function (item) {
-      if (item.dataset.direction) {
+      if (direction) {
+        item.style.setProperty('--animation-name', "fade-in-".concat(direction));
+      } else if (item.dataset.direction) {
         item.style.setProperty('--animation-name', "fade-in-".concat(item.dataset.direction));
       }
 
-      if (item.dataset.duration) {
+      if (duration) {
+        item.style.setProperty('--duration', duration);
+      } else if (item.dataset.duration) {
         item.style.setProperty('--duration', item.dataset.duration);
       }
 
-      if (item.dataset.easing) {
+      if (easing) {
+        item.style.setProperty('--easing', easing);
+      } else if (item.dataset.easing) {
         item.style.setProperty('--easing', item.dataset.easing);
       }
 
-      observer.observe(item);
+      if (observer) {
+        observer.observe(item);
+      }
     });
   }
 
